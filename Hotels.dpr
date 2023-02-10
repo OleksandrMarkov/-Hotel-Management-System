5e8165b1fdc1968fa838b595a6b4684fba95a9ab
Oleksandr Markov <ol.markov1999@gmail.com>
2023-02-10 20:07:19 +0200

    new project init

---
+++ Hotels.dpr
@@ -0,0 +1,14 @@
+program Hotels;
+
+uses
+  Vcl.Forms,
+  Hotel in 'Hotel.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
